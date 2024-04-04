650f7cdfd17d61020a68c08c1c7f660df6cd1252
Subhransu Sekhar Rout <155973959+subhransu-max@users.noreply.github.com>
2024-04-04 16:04:51 +0530

    Create skipcharacter.java

---
+++ skipcharacter.java
@@ -0,0 +1,17 @@
+public class skipcharacter {
+    public static void main(String[] args) {
+      skip("","chhadah");
+    }
+    static void skip(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch= up.charAt(0);
+        if(up.charAt(0)== 'a'){
+            skip(p,up.substring(1));
+        }else {
+            skip(p+ch,up.substring(1));
+        }
+    }
+}
