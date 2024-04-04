8dd02b0c6cdf1bb286190cf755389c3208f2acf1
Subhransu Sekhar Rout <155973959+subhransu-max@users.noreply.github.com>
2024-04-04 16:24:15 +0530

    Create skipString.java

---
+++ skipString.java
@@ -0,0 +1,16 @@
+public class skipString {
+    public static void main(String[] args) {
+        System.out.println(skip("chappledgh"));
+    }
+    static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        char ch= up.charAt(0);
+        if(up.startsWith("apple")){
+            return skip(up.substring(5));
+        }else {
+            return up.charAt(0)+skip(up.substring(1));
+        }
+    }
+}
