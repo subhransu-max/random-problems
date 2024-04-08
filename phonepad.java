7be63f9471c02544945d7e42092570adc74accc0
Subhransu Sekhar Rout <155973959+subhransu-max@users.noreply.github.com>
2024-04-08 15:36:56 +0530

    Update phonepad.java

--- phonepad.java
+++ phonepad.java
@@ -4,6 +4,21 @@ public class phonepad {
     public static void main(String[] args) {
         padvoid("","12");
         System.out.println(pad("","12"));
+        System.out.println(count("","12"));
+    }
+
+    private static int count(String p, String up) {
+        if(up.isEmpty()){
+            return 1;
+        }
+        int c= up.charAt(0)-'0';
+        int count =0;
+        for (int i = (c-1)*3; i < c*3; i++) {
+            char ch = (char)('a'+i);
+           count =count + count(p+ch, up.substring(1));
+        }
+        return count;
+
     }
 
     public static void padvoid(String p, String up) {
